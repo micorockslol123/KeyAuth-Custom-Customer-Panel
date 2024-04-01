b378dd6e97629f38a8febd3b1851751304be3d98
cacomoel <157077910+cacomoel@users.noreply.github.com>
2024-04-01 00:22:29 -0400

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,4 +1,4 @@
 <?php
-$name = ""; // the name of your application
-$OwnerId = ""; // can be found in your account settings
-$SellerKey = ""; // application's seller key. Found in seller settings (You must have seller plan)
+$name = "anubis"; // the name of your application
+$OwnerId = "UX5UfpdK0z"; // can be found in your account settings
+$SellerKey = "043360d6bf7dabf74892605170b63b05"; // application's seller key. Found in seller settings (You must have seller plan)
